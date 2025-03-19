e9278d5a0a72b44eb2dbe97f49369b63d4d8019f
Dwan Veloza <dwanveloza@gmail.com>
2025-03-18 19:37:49 -0500

    (fix): conection axios

--- src/infrastructure/externals/api/apiClient.ts
+++ src/infrastructure/externals/api/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+import https from "https";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
   headers: {
     "Content-Type": "application/json",
   },
