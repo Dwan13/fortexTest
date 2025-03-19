3eeee14f94b848277f81cfe4cd57ee3a0c17048c
Dwan Veloza <dwanveloza@gmail.com>
2025-03-18 21:01:12 -0500

    ajuste api

--- src/infrastructure/externals/api/apiClient.ts
+++ src/infrastructure/externals/api/apiClient.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
-import https from "https";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
   headers: {
     "Content-Type": "application/json",
   },
